c001e6c3c7a4dbabb50650bad512a3d99dfef51c
AndreaZanfretta <andrea.zanfretta.97@gmail.com>
2022-06-16 17:28:40 +0200

    bonus fatto

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,20 @@
-console.log('ciao')
-document.getElementById("delete").addEventListener("click", function(e){
-    e.preventDefault();
-    prompt('ciao')
-    console.log('ciao')
-});
+/* 
+let del = document.getElementsByClassName('del');
+
+del.forEach(buttons => {
+    buttons.addEventListener('click', function(e){
+        console.log(buttons)
+    })
+}); */
+document.querySelectorAll('.delete').forEach(button => {
+    button.addEventListener('click', function(e){
+        
+        res = prompt('Sei sicuro di volerlo cancellare? si/no');
+        if(res === 'si'){
+            
+        }else{
+            e.preventDefault();
+        }
+    });
+  });
+
